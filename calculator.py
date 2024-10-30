8c9329225f91b2421684d6c567529ab245e717f2
Haymanot Asmare <hasmare463@gimal.com>
2024-10-30 15:03:08 -0700

    update some code

--- calculator.py
+++ calculator.py
@@ -7,7 +7,7 @@ class calculator:
         master.resizable(False,False)
         self.equation=StringVar()
         self.entry_value=''
-        Entry(width=17,bg='#fff',font=('Arial bold ',28),textvariable=self.equation).place(x=0,y=0)
+        Entry(width=17,bg='#ccddff',font=('Arial bold ',28),textvariable=self.equation).place(x=0,y=0)
         Button(width=11,height=4,text='(',relief='flat',bg='white',command=lambda:self.show('(')).place(x=0,y=50)
         Button(width=11,height=4,text=')',relief='flat',bg='white',command=lambda:self.show(')')).place(x=90,y=50)
         Button(width=11,height=4,text='%',relief='flat',bg='white',command=lambda:self.show('%')).place(x=180,y=50)
@@ -22,11 +22,11 @@ class calculator:
         Button(width=11,height=4,text='9',relief='flat',bg='white',command=lambda:self.show(9)).place(x=90,y=275)
         Button(width=11,height=4,text='0',relief='flat',bg='white',command=lambda:self.show(0)).place(x=90,y=350)
         Button(width=11,height=4,text='.',relief='flat',bg='white',command=lambda:self.show('.')).place(x=180,y=350)
-        Button(width=11,height=4,text='+',relief='flat',bg='white',command=lambda:self.show('+')).place(x=270,y=350)
+        Button(width=11,height=4,text='+',relief='flat',bg='white',command=lambda:self.show('+')).place(x=270,y=275)
         Button(width=11,height=4,text='-',relief='flat',bg='white',command=lambda:self.show('-')).place(x=270,y=200)
         Button(width=11,height=4,text='/',relief='flat',bg='white',command=lambda:self.show('/')).place(x=270,y=50)
         Button(width=11,height=4,text='x',relief='flat',bg='white',command=lambda:self.show('*')).place(x=270,y=125)
-        Button(width=11,height=4,text='=',relief='flat',bg='white',command=lambda:self.solve).place(x=270,y=350)
+        Button(width=11,height=4,text='=',relief='flat',bg='lightblue',command=self.solve).place(x=270,y=350)
         Button(width=11,height=4,text='c',relief='flat',command=self.clear).place(x=0,y=350)
     def show(self,value):
         self.entry_value+=str(value)
